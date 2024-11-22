36896768ddbe9a23a0bfc99391cad5ef9b3d1790
Olivier Blangéro <79638937+o-blangero-lidem@users.noreply.github.com>
2024-11-22 11:28:30 +0100

    Composer et autoload

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello world !';
+// Chargement du système d'autoload
+require_once '../vendor/autoload.php';
+
+// Déclaration des classes que l'on va utiliser dans le fichier
+use App\App;
+
+$truc = new App();
+
+$truc->toto();
---
+++ app/src/App.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Classe de démarrage de l'application
+ */
+
+// Déclaration du namespace de ce fichier
+namespace App;
+
+class App
+{
+    public function toto(): void
+    {
+        echo 'Je suis Toto !';
+    }
+}
